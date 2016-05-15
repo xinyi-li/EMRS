24c7f4119fe21e16293f6a91b4be3aad71a27d82
xinyi-li <root@lxyhbase.com>
2016-05-15 15:37:47 +0800

    save

--- serverStart.js
+++ serverStart.js
@@ -1,23 +1,50 @@
 var express = require('express');
 var app= express();
 var path = require('path');
-app.listen(3000);
+app.listen(3001);
 
-app.set('views', './app/views/pages');
 app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'public')))
-app.locals.moment=require('moment')
+app.set('views', __dirname + '/app/views/pages');
+app.use(express.static(path.join(__dirname, '/app/public')));
+app.locals.moment=require('moment');
+
+var title='QM EMS System'
 
 app.get('/', function (req, res) {
   res.render('index', {
-  	title: 'QM EMS System'
+  	title: title
   });
 });
 
-app.get('/signIn', function(req, res){
-	res.render('signIn', {
-		title:'QM EMS System'
+app.get('/signInDoctor', function(req, res){
+	res.render('signInDoctor', {
+		title: title
 	});
 });
 
+app.get('/signInPatient', function(req, res){
+	res.render('signInPatient', {
+		title:title
+	});
+});
+
+app.get('/patientInfo', function(req, res){
+	res.render('patientInfo', {
+		title: title,
+		user: {id: "1", lastName: "xinyi", firstName:"li", mobileNo:+8615828006196, gender: "female", birth: "1994.08.08", email:"396275915@qq.com", createAt:"2015.01.01", updateAt:"2016.02.01"}
+	})
+})
+
+app.get('/medicalRecords', function(req, res){
+	res.render('medicalRecords', {
+		title: title,
+		records: [
+		{id: "1", doctorLastName: "xinyi", doctorFirstName:"li", hospital:"li"},
+		{id: "1", doctorLastName: "xinyi", doctorFirstName:"li", hospital:"li"},
+		{id: "1", doctorLastName: "xinyi", doctorFirstName:"li", hospital:"li"},
+		]
+	})
+})
+
+console.log(path.join(__dirname, 'public'));
 console.log("server start at port 3000");
